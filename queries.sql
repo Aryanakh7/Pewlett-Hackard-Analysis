c5a3788be855ba088eb990bd48380952963c9806
Aryanakh7 <Aryanakh7@users.noreply.github.com>
2021-02-13 00:13:20 -0500

    Challenge Final Analysis

--- queries.sql
+++ queries.sql
@@ -85,3 +85,66 @@ WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT * FROM retirement_info;
+
+
+
+
+-- Challenge 
+
+
+SELECT e.emp_no,
+e.first_name,
+e.last_name,
+ti.title,
+ti.from_date,
+ti.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+Order BY e.emp_no;
+
+SELECT * FROM retirement_titles
+
+-- Use Dictinct with Orderby to remove duplicate rows
+
+
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
+
+SELECT * FROM unique_titles
+
+-- Retrieve employees number of employees about to retire 
+SELECT COUNT (emp_no), 
+title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT (title) DESC;
+
+SELECT * FROM retiring_titles
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+ti.title
+INTO mentorship_eligibility
+FROM employees as e 
+INNER JOIN dept_emp as de
+on (e.emp_no = de.emp_no)
+INNER JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE (de.to_date = '9999-01-01') AND
+(e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+Order BY e.emp_no;
+
+SELECT * FROM mentorship_eligibility
